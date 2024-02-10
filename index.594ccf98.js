e7fe142e3a9ad160d4fb9c93b4f2b0aa982cd155
Wita-Shchurko <vitaliya.mememe@gmail.com>
2024-02-10 19:20:19 +0200

    make build

---
+++ index.594ccf98.js
@@ -0,0 +1,2 @@
+!function e(t,n){let r=document.createElement("ul");for(let t in n){let l=document.createElement("li");l.textContent=t,r.append(l),0!==Object.keys(n[t]).length&&e(l,n[t])}t.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.594ccf98.js.map
